501ec9219685d93c26c77869e18409124f744743
Monil Bansal <bansalmonil7@gmail.com>
2020-08-18 17:02:48 +0530

    'staff'

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var cookieParser          = require('cookie-parser');
 var session               = require('express-session');
 var Hospital              = require("./models/hospital")
 var User                  = require('./models/user')
+var Staff                  = require('./models/hospitalStaff')
 var passport              = require('passport');
+var passport2             = require('passport');
 mongoose.connect("mongodb+srv://Avinash:Avinash@1@cluster0.pqqse.mongodb.net/db?retryWrites=true&w=majority",{useNewUrlParser:true});
 
 // BodyParser middleware
@@ -14,35 +16,73 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+var main =  express();
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+main.use(cookieParser());
+
 
 //Express Session
 app.use(session({
   secret: 'secret',
-  saveUninitialized: false,
-  resave: false
+  saveUninitialized: true,
+  resave: true
 }));
 
 //Passport init
 app.use(passport.initialize());
 app.use(passport.session());
 
+main.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+//Passport init
+main.use(passport2.initialize());
+main.use(passport2.session());
+
 //Register User
 app.post('/register', function(req, res){
     console.log(req.body);
     var users = new User({
-        name: req.body.name,
-        bloodgrp:  req.body.bloodgrp,
+        name:       req.body.name,
+        bloodgrp:   req.body.bloodgrp,
         insurance:  req.body.insurance,
-        age:  req.body.age,
-        phone:  req.body.phone,
-        history:  req.body.history,
+        age:        req.body.age,
+        phone:      req.body.phone,
+        history:    req.body.history,
         emergencyContact:[],
         emergencyContactFor:[],
-        password: req.body.password
+        password:  req.body.password
       })
     User.createUser(users, function(err, user){
       if(err) throw err;
       User.create(user,function(err,newUser){
+	    	if(err){
+	            console.log(err);
+	    		return res.status(500);
+	        }
+	        console.log(newUser);
+	    	res.status(200).send(newUser).end()
+	    });
+    });
+});
+
+main.post('/register', function(req, res){
+    console.log(req.body);
+    console.log("ASDAD");
+    var users = new Staff({
+        name:       req.body.name,
+        hospital :  req.body.hospital,
+        age:        req.body.age,
+        phone:      req.body.phone,
+        password:  req.body.password
+      })
+    Staff.createStaff(users, function(err, user){
+      if(err) throw err;
+      Staff.create(user,function(err,newUser){
 	    	if(err){
 	            console.log(err);
 	    		return res.status(500);
@@ -77,12 +117,47 @@ passport.use(new LocalStrategy({
   }
 ));
 
+var LocalStrategy2 = require('passport-local').Strategy;
+passport2.use(new LocalStrategy2({
+	usernameField : 'phone',
+	passwordField : 'password'
+ 	},
+  function(phone, password, done) {
+    Staff.getStaffByPhone(phone, function(err, user){
+      if(err) throw err;
+      if(!user){
+        return done(null, false, {message: 'Unknown User'});
+      }
+      Staff.comparePassword(password, user.password, function(err, isMatch){
+        if(err) throw err;
+     	if(isMatch){
+     		console.log('MONIL');
+     	  return done(null, user);
+     	} else {
+     	  return done(null, false, {message: 'Invalid password'});
+     	}
+     });
+   });
+  }
+));
+
+
 passport.serializeUser(function(user, done) {
   done(null, user.phone);
 });
 
 passport.deserializeUser(function(phone, done) {
   User.getUserByPhone(phone, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport2.serializeUser(function(user, done) {
+  done(null, user.phone);
+});
+
+passport2.deserializeUser(function(phone, done) {
+  Staff.getStaffByPhone(phone, function(err, user) {
     done(err, user);
   });
 });
@@ -108,6 +183,40 @@ app.get('/logout', function(req, res){
 });
 
 
-app.listen(3000, function(){
+// Endpoint to login
+main.post('/staff/login', passport.authenticate('local'),
+  function(req, res) {
+    res.send(req.user);
+  }
+);
+
+// Endpoint to get current user
+main.get('/staff', function(req, res){
+	console.log(req.user);
+  	res.send(req.user).end();
+})
+
+
+// Endpoint to logout
+main.get('/staff/logout', function(req, res){
+  req.logout();
+  res.send(null)
+});
+
+app.get('/',function(req,res){
+  Hospital.find({},function(err,hospitals){
+		if(err)
+		{
+			console.log(err);
+			res.send("null");
+		}
+		else res.send(hospitals);
+		});
+})
+
+app.listen(3001, function(){
+    console.log("The Server Has Started!");
+ });
+main.listen(3000, function(){
     console.log("The Server Has Started!");
  });
---
+++ models/hospitalStaff.js
@@ -0,0 +1,33 @@
+var mongoose=require("mongoose");
+var bcrypt = require('bcryptjs');
+
+var StaffSchema=new mongoose.Schema({
+    name:String,
+    age:Number,
+    phone:Number,
+    password : String,
+    hospital : String
+});
+
+var Staff = module.exports=mongoose.model("staff",StaffSchema);
+
+module.exports.createStaff = function(newStaff, callback){
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(newStaff.password, salt, function(err, hash) {
+      newStaff.password = hash;
+      newStaff.save(callback);
+    });
+  });
+}
+
+module.exports.getStaffByPhone = function(phone, callback){
+  var query = {phone: phone};
+  Staff.findOne(query, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+  bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    if(err) throw err;
+    callback(null, isMatch);
+  });
+}
